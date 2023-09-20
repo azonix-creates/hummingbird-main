2bebb1c69e7d832dbe381b12da9a70c0c4bb5186
azonix <73659355+azonix-creates@users.noreply.github.com>
2023-09-19 20:09:44 -0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -3,14 +3,8 @@ import express, { Express, request, response } from "express";
 
 const app: Express = express();
 
-// add a route that lives separately from the SvelteKit app
-app.get('/healthcheck', (req, res) => {
-	res.end('ok');
-});
-
-// let SvelteKit handle everything else, including serving prerendered pages and static assets
 app.use(handler);
 
-app.listen(80, () => {
+app.listen(80, () => { // runs on default port but doesnt rlly matter simce this is deployed to vercel
 	console.log('listening on port 3000');
 });
